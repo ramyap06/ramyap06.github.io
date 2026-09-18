87d244d6018d2f935ed18991bb9ac9e9a307a830
Ramya Prasanna <prasannr@purdue.edu>
2026-09-18 13:18:52 -0400

    updated about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,6 +12,7 @@ function About() {
       <div className="about-text">
         <h2>About Me</h2>
         <p>
+          I focus on full-stack development and AI/ML, building impact-driven, human-facing products across industries like healthcare, engineering, and finance. I love taking on new problems, learning new technologies, and exploring different areas of computer science.I also enjoy systems and low-level engineering, where I’ve built a shell, memory allocator, and HTTP server from scratch in C. If any of that sounds interesting, feel free to keep exploring my space! 💖
         </p>
       </div>
     </section>
